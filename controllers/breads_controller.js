5f951f0f41b02ee57dbd5b1e5e476069aa6d4990
danielf9403 <103592213+danielf9403@users.noreply.github.com>
2022-08-30 15:10:23 -0400

    sending index and show data

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -4,8 +4,27 @@ const Bread = require('../models/bread.js')
 
 // INDEX
 breads.get('/', (req, res) => {
-  res.send(Bread)
+    res.render('Index',
+    {
+        breads: 'data from our models',
+        username: 'John doE',
+        asdf: 'jkl;'
+      }
+      
+    )
+  // res.send(Bread)
 })
+
+// INDEX
+breads.get('/', (req, res) => {
+    res.render('Index',
+      {
+        breads: Bread
+      }
+    )
+  // res.send(Bread)
+})
+
 
 // SHOW
 breads.get('/:arrayIndex', (req, res) => {
